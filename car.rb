dce544c7db99125db66f7b75916f0bc5d4172b68
Carlos Barbiero <carlosbarbiero@gmail.com>
2019-04-03 12:14:20 -0300

    implemented the solution

--- car.rb
+++ car.rb
@@ -1,6 +1,71 @@
 # frozen_string_literal: true
 
 class Car
+  attr_reader :year, :make, :model, :trim
 
+  def initialize(attrs = {})
+    @year  = attrs[:year]
+    @make  = attrs[:make]
+    @model = attrs[:model]
+    @trim  = attrs[:trim]
+  end
+
+  # Normalize all cars data following respective rules
+  # and get them as a hash
+  def normalize
+    format_year
+    format_make
+    format_model
+    format_trim
+
+    instance_variables.each_with_object({}) do |v, h|
+      h[v.to_s.delete('@').to_sym] = instance_variable_get(v)
+    end
+  end
+
+  private
+
+  # Car year format. Applied rules:
+  # - Valid years are from 1900 until 2 years in the future from today
+  # - A value that can't be normalized should be returned as is
+  #
+  def format_year
+    int_year = @year.to_i
+    @year = int_year if int_year.between?(1900, Time.now.year + 2)
+  end
+
+  # Car make format. Applied rules:
+  # - Typos & abbreviations corrected
+  #
+  def format_make
+    case @make
+    when 'fo', 'ford'
+      @make = 'Ford'
+    when 'Chev'
+      @make = 'Chevrolet'
+    end
+  end
+
+  # Car model format. Applied rules:
+  # - Sometimes the trim of a vehicle will be provided in the "model" field
+  # and will need to be extracted to the "trim" field
+  #
+  def format_model
+    models = @model.split(' ')
+    @trim = models[1].upcase if models.length > 1
+    @model = models[0].capitalize unless models[0] == 'foo'
+  end
+
+  # Car trim (different features or packages for the same model of vehicle) format
+  # Applied rules:
+  # - The word "blank" should be returned as nil
+  #
+  def format_trim
+    case @trim
+    when 'blank'
+      @trim = nil
+    when 'st'
+      @trim = 'ST'
+    end
+  end
 end
-
---
+++ normalize.rb
@@ -0,0 +1,27 @@
+require_relative 'car'
+
+def normalize_data(input)
+  car = Car.new(input)
+  car.normalize
+end
+
+examples = [
+  [{ :year => '2018', :make => 'fo', :model => 'focus', :trim => 'blank' },
+  { :year => 2018, :make => 'Ford', :model => 'Focus', :trim => nil }],
+  [{ :year => '200', :make => 'blah', :model => 'foo', :trim => 'bar' },
+   { :year => '200', :make => 'blah', :model => 'foo', :trim => 'bar' }],
+  [{ :year => '1999', :make => 'Chev', :model => 'IMPALA', :trim => 'st' },
+   { :year => 1999, :make => 'Chevrolet', :model => 'Impala', :trim => 'ST' }],
+  [{ :year => '2000', :make => 'ford', :model => 'focus se', :trim => '' },
+   { :year => 2000, :make => 'Ford', :model => 'Focus', :trim => 'SE' }]
+]
+
+examples.each_with_index do |(input, expected_output), index|
+  output = normalize_data(input)
+
+  if output != expected_output
+    puts "Example #{index + 1} failed,
+          Expected: #{expected_output.inspect}
+          Got:      #{output.inspect}"
+  end
+end
